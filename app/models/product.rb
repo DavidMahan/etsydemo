632e6b312fbdee1fc5d2fa42e92cac159b74e26c
David Mahan <davidmahan@my.unt.edu>
2016-09-21 13:16:56 -0500

    moved validator code, but still didn't fix it

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
 	has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg"
-end
+
+  validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
+
+
+  end
