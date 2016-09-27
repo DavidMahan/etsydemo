1300d187d564284ba8ef22e026490f26fd62e3a7
David Mahan <davidmahan@my.unt.edu>
2016-09-26 19:08:09 -0500

    Add validations for Users and Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ActiveRecord::Base
-	has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg"
+	has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
 
   validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
 
+	validates :name, :description, :price, presence: true
+  	validates :price, numericality: { greater_than: 0 }
+  	validates_attachment_presence :image
 
   end
